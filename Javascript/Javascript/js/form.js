e0b121aaa067bbb8d932041051c8938fbd984d61
lais pereira dos santos <lais.pereira.santos92@gmail.com>
2020-06-05 19:35:55 -0300

    Validação do formulário

--- Javascript/Javascript/js/form.js
+++ Javascript/Javascript/js/form.js
@@ -7,15 +7,36 @@ botaoAdicionar.addEventListener("click", function () {
 
     
     var pacienteTr = montaTr(paciente);
-    
-    //adicionando o paciente na tabela
+
+    var erros = validaPaciente(paciente);
+    console.log(erros);
+    if(erros.length > 0){
+        exibeMensagensDeErro(erros);
+       
+        return; 
+    }
+
+        var pacienteTr = montaTr(paciente);
     var tabela = document.querySelector("#tabela-pacientes");
 
     tabela.appendChild(pacienteTr);
 
     form.reset();
+    var mensagensErro = document.querySelector("#mensagens-erro");
+    mensagensErro.innerHTML = "";
 
 });
+
+function exibeMensagensDeErro(erros) {
+    var ul = document.querySelector("#mensagens-erro");
+    ul.innerHTML = "";
+    erros.forEach(function(erro) {
+        var li = document.createElement("li");
+        li.textContent = erro;
+        ul.appendChild(li);
+        
+    });
+}
 
 titulo.addEventListener("click", function () {
     console.log("fui clicado");
@@ -55,4 +76,34 @@ function montaTd(dado,classe) {
     td.classList.add(classe);
 
     return td;
+}
+
+function validaPaciente(paciente) {
+    var erros = [];
+    
+    if( paciente.nome.length == 0) {
+        erros.push("O nome não pode ficar em branco");
+    }
+
+    if(!validaPeso(paciente.peso)) {
+        erros.push("Peso é inválido");
+    }
+
+    if(!validaAltura(paciente.altura)) {
+        erros.push("Altura é inválida");
+        
+    }
+   
+    if( paciente.gordura.length == 0) {
+        erros.push(" A gordura não pode ficar em branco");
+    }
+
+    if(paciente.peso.length == 0) {
+        erros.push(" O peso não pode ficar em branco")
+    }
+
+    if(paciente.altura.length == 0) {
+        erros.push(" A altura não pode ficar em branco")
+    }
+    return erros;
 }
